b2a43933dae2a0f2e9c7aec535e339f2ed279f42
bioinfowheat <maxlongbow@gmail.com>
2026-03-31 05:57:38 -0700

    update

--- wheat_work/run1/CRISPResso75.sh
+++ wheat_work/run1/CRISPResso75.sh
@@ -3,30 +3,38 @@ cd /mnt/griffin/chrwhe/ONT_testing/espresso75
 cp /mnt/griffin/4interns/combined_fastq_files/*gz .
 parallel 'unpigz {}' ::: *.gz 
 
+# install 
+micromamba create -n crispresso2_env -c conda-forge -c bioconda crispresso2
+# activate installation
 micromamba activate crispresso2_env
 
-# files
+# file names
 barcode01.combined.fastq
 
+# can I list them easily
 for x in {01..75}; do
     echo "barcode$x" "barcode$x.combined.fastq"
 done | tr " " "\t" > NSPbatch75.batch
 
+# add a header to the batch file
 nano NSPbatch75.batch
 name    fastq_r1
 barcode01       barcode01.run1.fastq
 barcode02       barcode02.run1.fastq
 
-# batch run 
+# batch run on raw fastq data
 CRISPRessoBatch --batch_settings NSPbatch75.batch --amplicon_seq ACGAAAAGTCATGGCAAGTCATAGAAAGATCGCGAAAAATACTAATTAATGATTTTGAAAATGATAGGATCTGATTGTTTCCTTGATAACATTTGAATATATAAACTCGACACCTGTGAAATAAATCAATTAACACTAATAAAATGAAAGGTGTTGTAGTCTTCTTAGCCCTCGCGGCTTTGGGGAGCGCTAAGCCTCGTCTGTTCGAGACGTTCCAAGACCACTTTCAACACTTTTTGGACATAAGCAATGCCTTGGAGGGTGCCCATTGGAGACGACAACAGGGTCAGGGGTACACTCCTAACCCAGAATACATCGAAATGTTGAACAAGCTGAGCGAGCAAAACTTGAAACAGATGCTCGCTGATTTAAAAAAGGAGCCTGAAGTGCAAGACGTGAGTACCTTATACATTTATATACTAGCCTCAAAATTGTTTGAAATAAATATTTTTACAAAGAATAACTGATTCGATTTGGACATTGCACAACCTCGTT -p 40 -g CTTCTTAGCCCTCGCGGCTTTGG -wc -3 -w 10 --zip_output --plot_window_size 30 --place_report_in_output_folder
 
 scp chrwhe@duke.zoologi.su.se:/mnt/griffin/chrwhe/ONT_testing/espresso75/CRISPRessoBatch_on_NSPbatch75.zip .
 
-
+########
+# batch run on 2nd version of fastp cleaning
+# refresh my memmory
 cd fp2_clean
 cp /mnt/griffin/4interns/combined_fastq_files/*gz .
 ls *| cut -f1 -d '.' > 75barcodes
 
+# how were these cleaned?
 cat 75barcodes | parallel -j 13 '../../software/fastp/fastp --in1 {}.combined.fastq.gz \
   --out1 fastp_second_run/{}.combined.trimmed2.fastq.gz \
   --average_qual 10 \
@@ -40,12 +48,14 @@ cat 75barcodes | parallel -j 13 '../../software/fastp/fastp --in1 {}.combined.fa
   --cut_front_window_size 10 --cut_right_window_size 10 \
   --cut_front_mean_quality 10 --cut_right_mean_quality 10 '
 
-# clead up, removed raw files, etc
-
-
+# OK, now I know what I have, so make new folder
+mkdir espresso75_fp2
+# go into it
 cd espresso75_fp2
+# get new clean reads into it
 cp ../fp2_clean/*.gz .
 
+# make new batch file based upon the new fastq names
 for x in {01..75}; do
     echo "barcode$x" "barcode$x.combined.trimmed2.fastq.gz"
 done | tr " " "\t" > NSPbatch75.trimmed.batch
@@ -56,6 +66,7 @@ name    fastq_r1
 # run
 CRISPRessoBatch --batch_settings NSPbatch75.trimmed.batch --amplicon_seq ACGAAAAGTCATGGCAAGTCATAGAAAGATCGCGAAAAATACTAATTAATGATTTTGAAAATGATAGGATCTGATTGTTTCCTTGATAACATTTGAATATATAAACTCGACACCTGTGAAATAAATCAATTAACACTAATAAAATGAAAGGTGTTGTAGTCTTCTTAGCCCTCGCGGCTTTGGGGAGCGCTAAGCCTCGTCTGTTCGAGACGTTCCAAGACCACTTTCAACACTTTTTGGACATAAGCAATGCCTTGGAGGGTGCCCATTGGAGACGACAACAGGGTCAGGGGTACACTCCTAACCCAGAATACATCGAAATGTTGAACAAGCTGAGCGAGCAAAACTTGAAACAGATGCTCGCTGATTTAAAAAAGGAGCCTGAAGTGCAAGACGTGAGTACCTTATACATTTATATACTAGCCTCAAAATTGTTTGAAATAAATATTTTTACAAAGAATAACTGATTCGATTTGGACATTGCACAACCTCGTT -p 50 -g CTTCTTAGCCCTCGCGGCTTTGG -wc -3 -w 10 --zip_output --plot_window_size 30 --place_report_in_output_folder
 
+# get to local computer to look at the html file
 scp chrwhe@duke.zoologi.su.se:/mnt/griffin/chrwhe/ONT_testing/espresso75_fp2/CRISPRessoBatch_on_NSPbatch75.trimmed.zip .
 
 
